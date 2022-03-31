b58613e84c6a229a2a167caa045e53e48040606a
tayni <smizerovskiy@gmail.com>
2022-03-31 13:45:24 -0400

    Initial commit

---
+++ src/InsuredShippingItem.java
@@ -0,0 +1,23 @@
+public class InsuredShippingItem extends ShippingItem
+{
+  private double insuredAmount;
+  
+  public InsuredShippingItem(double w, double insAmt)
+  {
+    /* TO BE IMPLEMENTED IN PART B */
+    super(w);
+    insuredAmount = insAmt;
+
+  }
+  
+  public double getCost()
+  {
+    /* TO BE IMPLEMENTED IN PART C */
+    return super.getCost() + insuredAmount;
+  }
+  
+  public void addMoreInsurance(double amt)
+  {
+    insuredAmount += amt;
+  }
+}
